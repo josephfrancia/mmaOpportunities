558daf59ca7696f35a2d9cb1740ffff8fa9d18ab
Joseph Andrew Francia <josephandrewfrancia@josephs-mbp-2.lan>
2020-06-11 19:46:21 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+import requests 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+import time as t
+
+from sportsbooks import cleanMoneylineData
+from sportsbooks import getJuice
+from sportsbooks import getCasinoProfit
+from sportsbooks import getImpliedOdds
+
+#draftkings
+draftkings_url = 'https://sportsbook.draftkings.com/leagues/mma/2162?category=fight-lines&subcategory=moneyline'
+draftkings_soup_object = BeautifulSoup(requests.get(draftkings_url).text)
+draftkings_names_html = draftkings_soup_object.select(".sportsbook-outcome-cell__label")
+draftkings_odds_html = draftkings_soup_object.select(".sportsbook-outcome-cell__element")
+draftkings_names = [x.text for x in draftkings_names_html]
+draftkings_odds = [int(x.text) for x in draftkings_odds_html if x.text != '']
+
+draftkings_df = cleanMoneylineData(draftkings_names, draftkings_odds, "draftkings") 
+
+#fanduel
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://sportsbook.fanduel.com/sports/navigation/7287.1/9886.3")
+fanduel_raw_data = driver.find_elements_by_class_name('selection-column')
+fanduel_data = [x.text.split('\n') for x in fanduel_raw_data]
+fanduel_names = [x[0] for x in fanduel_data]
+fanduel_odds = [x[1] for x in fanduel_data]
+
+fanduel_df = cleanMoneylineData(fanduel_names, fanduel_odds, "fanduel") 
+
+#betonline
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.betonline.ag/sportsbook/martial-arts/mma")
+t.sleep(10)
+betonline_data_html = driver.find_elements_by_css_selector('.bdevtt')
+betonline_data = np.array([x.text for x in betonline_data_html])
+indices_to_remove = []
+for i in range(0, len(betonline_data)):
+    if pd.Series(betonline_data[i]).isin(["Un", "Ov"])[0]:
+        indices_to_remove.append([i, i+1, i+2, i+3])
+
+betonline_data = np.delete(betonline_data, indices_to_remove)
+betonline_odds = [x for x in betonline_data if bool(re.search("^\\+", x)) or bool(re.search("^\\-", x))]
+betonline_names = [x for x in betonline_data if re.sub('[\s+]', '', re.sub('-', '', x)).isalpha()
+                  and len(x) > 1 
+                  and x != "\no\n"
+                  and not bool(re.search("\\:", x))]
+
+betonline_df = cleanMoneylineData(betonline_names, betonline_odds, "betonline") 
+
+#bovada
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.bovada.lv/sports/ufc-mma")
+t.sleep(30)
+bovada_odds_html = driver.find_elements_by_css_selector('.bet-btn')
+bovada_names_html = driver.find_elements_by_css_selector('.competitor-name')
+bovada_odds = [100 if x.text == 'EVEN' else x.text for x in bovada_odds_html]
+bovada_names = [x.text for x in bovada_names_html]
+
+bovada_df = cleanMoneylineData(bovada_names, bovada_odds, "bovada") 
+
+#mybookie
+mybookie_url = 'https://mybookie.ag/sportsbook/ufc/'
+mybookie_soup_object = BeautifulSoup(requests.get(mybookie_url).text)
+mybookie_names_html = mybookie_soup_object.select(".text-truncate")
+mybookie_odds_html = mybookie_soup_object.select(".lines-odds")
+mybookie_names = np.array([x.text for x in mybookie_names_html])
+mybookie_odds = np.array([x.text for x in mybookie_odds_html])
+mybookie_odds = mybookie_odds[np.logical_not(pd.Series(mybookie_odds).isin([' - ', ' -  ']))]
+listOfOverUnders = ["&" in x for x in mybookie_odds]
+listOfOverUnders = [not x for x in listOfOverUnders]
+mybookie_odds = mybookie_odds[listOfOverUnders]
+mybookie_odds = mybookie_odds[0:76]
+mybookie_names = mybookie_names[0:76]
+
+mybookie_df = cleanMoneylineData(mybookie_names, mybookie_odds, "mybookie")
+
+#merging data 
+merged_data = pd.concat([bovada_df, draftkings_df, fanduel_df, mybookie_df, betonline_df])
+
+num_unique_sportsbooks = pd.DataFrame(merged_data.groupby(['team1', 'team2']).sportsbook.nunique())
+
+best_team1_ml_df = merged_data.groupby(['team1', 'team2'])['team1MoneyLine'].agg({'bestTeam1MoneyLine':'max'})
+best_team2_ml_df = merged_data.groupby(['team1', 'team2'])['team2MoneyLine'].agg({'bestTeam2MoneyLine':'max'})
+
+
+best_team1_ml_df['bestTeam1ImpliedOdds'] = best_team1_ml_df['bestTeam1MoneyLine'].apply(lambda x: getImpliedOdds(x))
+best_team2_ml_df['bestTeam2ImpliedOdds'] = best_team2_ml_df['bestTeam2MoneyLine'].apply(lambda x: getImpliedOdds(x))
+
+
+consensus_team1_prob = merged_data.groupby(['team1', 'team2'])['true_probability_team1_wins'].agg({'consensusTeam1Probability':'mean'})
+consensus_team2_prob = merged_data.groupby(['team1', 'team2'])['true_probability_team2_wins'].agg({'consensusTeam2Probability':'mean'})
+
+
+avg_team1_ml = merged_data.groupby(['team1', 'team2'])['team1MoneyLine'].agg({'average_team1_ml':'mean'})
+avg_team2_ml = merged_data.groupby(['team1', 'team2'])['team2MoneyLine'].agg({'average_team1_m2':'mean'})
+
+merged_data = pd.merge(merged_data, best_team1_ml_df, on=['team1', 'team2'], how='outer')
+merged_data = pd.merge(merged_data, num_unique_sportsbooks, on=['team1', 'team2'], how='outer')
+merged_data = pd.merge(merged_data, best_team2_ml_df, on=['team1', 'team2'], how='outer')
+merged_data = pd.merge(merged_data, consensus_team1_prob, on=['team1', 'team2'], how='outer')
+merged_data = pd.merge(merged_data, consensus_team2_prob, on=['team1', 'team2'], how='outer')
+merged_data = pd.merge(merged_data, avg_team1_ml, on=['team1', 'team2'], how='outer')
+merged_data = pd.merge(merged_data, avg_team2_ml, on=['team1', 'team2'], how='outer')
+merged_data['bestJuice'] = getJuice(np.array(merged_data['bestTeam1MoneyLine']), np.array(merged_data['bestTeam2MoneyLine']))
+
+merged_data['isBestTeam1MoneyLine'] = merged_data['bestTeam1MoneyLine'] == merged_data['team1MoneyLine']
+merged_data['isBestTeam2MoneyLine'] = merged_data['bestTeam2MoneyLine'] == merged_data['team2MoneyLine'] 
+merged_data['providesValue'] = [x or y for x,y in zip(merged_data['isBestTeam1MoneyLine'], merged_data['isBestTeam2MoneyLine'])]
+merged_data['bestDeviationTeam1'] = [x - y for x,y in zip(merged_data['consensusTeam1Probability'], merged_data['bestTeam1ImpliedOdds'])]
+merged_data['bestDeviationTeam2'] = [x - y for x,y in zip(merged_data['consensusTeam2Probability'], merged_data['bestTeam2ImpliedOdds'])]
+
+for x in range(0, len(merged_data["team1"])):
+    if merged_data["bestDeviationTeam1"][x] > 0 and merged_data["bestDeviationTeam1"][x] > merged_data["bestDeviationTeam2"][x] and merged_data["team1MoneyLine"][x] == merged_data["bestTeam1MoneyLine"][x]: 
+        print("Bet on " + np.array(merged_data["team1"])[x] + " at " + str(np.array(merged_data["bestTeam1MoneyLine"])[x]) + " at " + np.array(merged_data["sportsbook_x"])[x] + " for deviation of " + str(np.array(merged_data["bestDeviationTeam1"])[x]))
+
+    if merged_data["bestDeviationTeam2"][x] > 0 and merged_data["bestDeviationTeam2"][x] > merged_data["bestDeviationTeam1"][x] and merged_data["team2MoneyLine"][x] == merged_data["bestTeam2MoneyLine"][x]: 
+        print("Bet on " + np.array(merged_data["team2"])[x] + " at " + str(np.array(merged_data["bestTeam2MoneyLine"])[x]) + " at " + np.array(merged_data["sportsbook_x"])[x] + " for deviation of " + str(np.array(merged_data["bestDeviationTeam2"])[x]))
+
+ 
+
+
---
+++ sportsbooks.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+import requests 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+import time as t
+
+def getImpliedOdds(moneyLine): 
+    if moneyLine >= 100:
+        impliedOdds = 100 / (100 + moneyLine)
+    elif moneyLine <= -100:
+        impliedOdds = abs(moneyLine) / (100 + abs(moneyLine))
+    else: 
+        impliedOdds = None
+    return(impliedOdds)
+
+def getCasinoProfit(moneyLines):
+    overround = sum(np.array([getImpliedOdds(x) for x in moneyLines])) - 1
+    vigorish = overround / (1 + overround)
+    return(vigorish)
+
+def getJuice(moneyLines1, moneyLines2):
+    juice = np.empty(len(moneyLines1), dtype=object)
+    for x in range(0,len(moneyLines1)):
+        juice[x] = getCasinoProfit([moneyLines1[x], moneyLines2[x]])
+    return(juice)
+
+def cleanMoneylineData(names, odds, sportsbook): 
+    team1Names = np.empty(len(names), dtype=object)
+    team1MoneyLine = np.empty(len(names), dtype=object)
+    team2Names = np.empty(len(names), dtype=object)
+    team2MoneyLine = np.empty(len(names), dtype=object)
+
+    for x in range(0, len(names)):
+        if x%2 == 0:
+            team1Names[x] = names[x]
+            team1MoneyLine[x] = odds[x]
+        else: 
+            team2Names[x] = names[x]
+            team2MoneyLine[x] = odds[x]
+
+    team1Names = [str(x).strip().upper() for x in team1Names if x is not None]
+    team2Names = [str(x).strip().upper() for x in team2Names if x is not None]
+    team1MoneyLine = [int(x) for x in team1MoneyLine if x is not None]
+    team2MoneyLine = [int(x) for x in team2MoneyLine if x is not None]
+    juice = getJuice(team1MoneyLine, team2MoneyLine)
+    true_probability_team1_wins = [getImpliedOdds(x) / (getImpliedOdds(x) + getImpliedOdds(y)) for x, y in zip(team1MoneyLine, team2MoneyLine)]
+    true_probability_team2_wins = [getImpliedOdds(x) / (getImpliedOdds(x) + getImpliedOdds(y)) for x, y in zip(team2MoneyLine, team1MoneyLine)]
+    
+    needs_switching = [x == max(x, y) for x, y in zip(team1Names, team2Names)]
+
+    team1NamesOrdered = [min(x, y) for x, y in zip(team1Names, team2Names)]
+    team2NamesOrdered = [max(x, y) for x, y in zip(team1Names, team2Names)]
+
+    team1MoneyLineOrdered = [y if z is True else x for x, y, z in zip(team1MoneyLine,  team2MoneyLine, needs_switching)]
+
+    team2MoneyLineOrdered = [x if z is True else y for x, y, z in zip(team1MoneyLine,  team2MoneyLine, needs_switching)]
+
+    true_probability_team1_wins_ordered = [y if z is True else x for x, y, z in 
+                                              zip(true_probability_team1_wins, 
+                                                  true_probability_team2_wins, 
+                                                  needs_switching)]
+
+    true_probability_team2_wins_ordered = [x if z is True else y for x, y, z in 
+                                              zip(true_probability_team1_wins, 
+                                                  true_probability_team2_wins, 
+                                                  needs_switching)]
+    
+    dictionary = {'team1': team1NamesOrdered, 
+                  'team2': team2NamesOrdered, 
+                  'team1MoneyLine': team1MoneyLineOrdered, 
+                  'team2MoneyLine': team2MoneyLineOrdered, 
+                  'juice': juice, 
+                  'true_probability_team1_wins': true_probability_team1_wins_ordered, 
+                  'true_probability_team2_wins': true_probability_team2_wins_ordered, 
+                  'sportsbook': sportsbook}
+    df = pd.DataFrame(dictionary)
+    return(df)
+    
